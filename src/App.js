660ca755f86db30cefccb2ebbb9ac74086335247
Chay <chaylin808@gmail.com>
2022-03-24 11:33:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import GlobalStyle from "./globalStyles";
+import About from "./pages/About";
+import Gmail from "./pages/Gmail";
+import Images from "./pages/Images";
+import Store from "./pages/Store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' exact element={<About />} />
+        <Route path='/store' exact element={<Store />} />
+        <Route path='/gmail' exact element={<Gmail />} />
+        <Route path='/images' exact element={<Images />} />
+      </Routes>
+    </>
   );
 }
 
